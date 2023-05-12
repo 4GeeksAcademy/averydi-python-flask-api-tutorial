a08e5e7e42929f0d0859817dd251adf1108f153d
David Avery <avery.di@gmail.com>
2023-05-12 22:00:27 +0000

    Flask turial

--- src/app.py
+++ src/app.py
@@ -11,9 +11,19 @@ def hello_world():
 def add_new_todo():
     request_body = request.json
     print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+     
+    todos.pop((position-1))
+    
+    return jsonify(todos)
 
 
-# These two lines should always be at the end of your app.py file.
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3245, debug=True)
+  app.run(host='0.0.0.0', port=3245, debug=True)
